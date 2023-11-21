bea3f5ce267cdbd69c4e111736fc38a22cec2a49
Makarov Igor <zurginq@gmail.com>
2023-11-21 19:56:36 +0400

    update

--- js/main.js
+++ js/main.js
@@ -1,34 +1,92 @@
-function openDB() {
-    return idb.openDB('db1', 1, {
-        upgrade(db) {
-            const store = db.createObjectStore('games', {
-                keyPath: 'id',
-                autoIncrement: true,
-            });
+function debounceFn(fn, wait) {
+    let timeoutId = null;
+    return function() {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => fn(...arguments), wait);
+    }
+}
+
+async function openDB() {
+    return idb.openDB('db1', 2, {
+        upgrade(db, oldVersion, newVersion, transaction, event) {
+            switch (newVersion) {
+                case 1:
+                    const games = db.createObjectStore('games', {
+                        keyPath: 'id',
+                        autoIncrement: true,
+                    });
+                case 2:
+                    db.createObjectStore('settings');
+            }
         },
     });
 }
 
-function loadGameById(gameId, callback) {
-    openDB().then(db => {
-        db.get('games', gameId).then(callback);
-    });
+async function incDBVersion(tx) {
+    const settingsStore = tx.objectStore('settings');
+    const version = await settingsStore.get('version');
+    return settingsStore.put(Number.isInteger(version) ? version + 1 : 1, 'version');
 }
 
-function updateGameId(gameId, update, done) {
-    openDB().then(db => {
-        const tx = db.transaction('games', 'readwrite');
-        tx.store.get(gameId).then(game => {
-            tx.store.put(update(game));
-        });
-        tx.done.then(done);
-    });
+async function loadGameById(gameId) {
+    return (await openDB()).get('games', gameId);
 }
 
-function deleteGame(gameId, callback) {
-    openDB().then(db => {
-        db.delete('games', gameId).then(callback);
-    });
+async function updateGameId(gameId, update) {
+    const db = await openDB();
+    const tx = db.transaction(['games', 'settings'], 'readwrite');
+    const gamesStore = tx.objectStore('games');
+    const game = await gamesStore.get(gameId);
+    await gamesStore.put(update(game));
+    await incDBVersion(tx);
+    return tx.done;
+}
+
+async function deleteGame(gameId) {
+    const db = await openDB();
+    const tx = db.transaction(['games', 'settings'], 'readwrite');
+    const gamesStore = tx.objectStore('games');
+    await gamesStore.delete(gameId);
+    await incDBVersion(tx);
+    return tx.done;
+}
+
+async function exportDb() {
+    const db = await openDB();
+    const tx = db.transaction(['games', 'settings'], 'readwrite');
+    const games = await tx.objectStore('games').getAll();
+    const settingKeys = await tx.objectStore('settings').getAllKeys();
+
+    const settings = {};
+    for (const key of settingKeys) {
+        settings[key] = await tx.objectStore('settings').get(key);
+    }
+
+    return {
+        games,
+        settings,
+    }
+}
+
+async function importDb(dbData) {
+    const db = await openDB();
+    const tx = db.transaction(['games', 'settings'], 'readwrite');
+    const gamesStore = tx.objectStore('games');
+    const settingsStore = tx.objectStore('settings');
+
+    await gamesStore.clear();
+    for (const game of dbData.games) {
+        await gamesStore.put(game);
+    }
+
+    await settingsStore.clear();
+    for (const [key, value] of Object.entries(dbData.settings)) {
+        await settingsStore.put(value, key);
+    }
+
+    return tx.done;
 }
 
 function getGameIdFromUrl() {
